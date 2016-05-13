3fd364952762df7e25915bf6348904c403fb34a6
Iain Collins <me@iaincollins.com>
2016-05-13 22:36:58 +0100

    Replace setTimeout in tests with more elegant test
    Now check to see if Express has added the routes yet and starts the tests only once they are loaded.

--- test/init.js
+++ test/init.js
@@ -1,14 +1,31 @@
 var request = require("supertest-as-promised");
 var app = require('../server.js');
 
-/**
- * This is a hack to add a short delay before running the tests.
- * The Express routes don't seem to be ready right away and I can't
- * find a suitable event to bind to that is fired when they are.
- */
 before(function(done) {
   this.timeout(1000 * 10);
-  setTimeout(function() {
-    done();
-  },1000);
+  
+  /**
+   * This checks looks to see if routes are loaded and ready to go before the
+   * tests start, otherwise there is a chance that routes defined with 
+   * express.Router() won't be ready and loaded.
+   */
+  var readyCheck = setInterval(function() {
+    var route, routes = [];
+    app._router.stack.forEach(function(middleware) {
+      if (middleware.route) {
+        routes.push(middleware.route);
+      } else if (middleware.name === 'router') {
+        middleware.handle.stack.forEach(function(handler) {
+          route = handler.route;
+          route && routes.push(route);
+        });
+      }
+    });
+  
+    if (routes.length > 1) {
+      clearInterval(readyCheck);
+      done();
+    }
+  },100);
+
 });
