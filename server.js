f331c3bc5203b5e1bfaf3f8d0f5872302a6bf678
Iain Collins <me@iaincollins.com>
2016-06-06 09:57:59 +0100

    Adding Access-Control-Allow-Headers header

--- server.js
+++ server.js
@@ -40,6 +40,7 @@ app.set('port', process.env.PORT || 3000);
 
 app.use(function(req, res, next) {
   res.setHeader("Access-Control-Allow-Origin", process.env.ALLOW_ORIGIN || "*");
+  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, X-Api-Key");
   res.setHeader("Access-Control-Allow-Methods", "INFO, GET, PUT, POST, DELETE, HEAD");
   res.locals.siteName = process.env.SITE_NAME || "Structured Data API";
   res.locals.baseUri = global.baseUri;
